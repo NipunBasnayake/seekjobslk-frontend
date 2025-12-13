7ffbfbcbbd9735a8824e781ec777cbc5a27909c7
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-12-14 01:51:15 +0530

    refactor: update JobList component grid layout for improved responsiveness

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -138,7 +138,7 @@ const JobList: React.FC<JobListProps> = ({ filters, jobs: parentJobs }) => {
 
       {/* Job Grid */}
       {!loading && !error && sortedJobs.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-6">
           {sortedJobs.map((job, index) => (
             <div
               key={job.id}
